71e5e0408d2011c328e74a371c4ff6f10da5f296
Teddy K <teddykoerniadi@gmail.com>
2024-12-18 11:49:18 +0700

    simplify database connection options in db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,11 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Database connected');
+    await mongoose.connect(process.env.MONGO_URI, {});
   } catch (err) {
     console.error(err.message);
     process.exit(1);
